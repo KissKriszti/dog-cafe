e337b45e834d3161a8de14ad1be0c50e98b1fae5
Kriszti <kecskeskriszti22@gmail.com>
2022-07-21 10:25:45 +0200

    intro page responsiveness

--- src/components/Headline/Welcome.jsx
+++ src/components/Headline/Welcome.jsx
@@ -31,7 +31,7 @@ export default function Welcome() {
                         modules={[Autoplay]}
                     >
                         {welcomeDogs.map((item, index) => (
-                            <SwiperSlide key={index}>
+                            <SwiperSlide key={index} rewind="true">
                                 <div className='imgContainer'>
                                     <img src={item} alt="dogs" className='imgBig' />
                                 </div>
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,22 +1,52 @@
+import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
 import { residentDogs } from '../../constants/imgList';
 import './intro.css';
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function Intro() {
+
+    const [dimensions, setDimensions] = useState(window.innerWidth)
+
+    useEffect(() => {
+        function handleResize() {
+            setDimensions(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+    }, []);
+
     return (
         <section className='sectionContainer'>
             <div className='residentContainer'>
                 <div className='noskew'>
                     <h2 className='residentTitle'>Residents</h2>
-                    <div className='dogContainer'>
-                        {residentDogs.map((item, index) => (
-                            <div style={{ position: "relative" }}>
-                                <img key={index} src={item.source} alt={item.name} className="residentImg" style={{ border: `6px solid ${item.color}` }} />
-                                <div className='nameContainer' style={{ backgroundColor: item.color }}>
-                                    <h4>{item.name}</h4>
+                    {dimensions <= 768 ?
+                        <div className='swiperContainer'>
+                            <Swiper navigation={true} rewind="true" modules={[Navigation]} className="" style={{ position: "relative" }}>
+                                {residentDogs.map((item, index) => (
+                                    <SwiperSlide key={index}>
+                                        <img src={item.source} alt={item.name} className="residentImg" style={{ border: `6px solid ${item.color}` }} />
+                                        <div className='nameContainer' style={{ backgroundColor: item.color }}>
+                                            <h4>{item.name}</h4>
+                                        </div>
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </div>
+                        :
+                        <div className='dogContainer'>
+                            {residentDogs.map((item, index) => (
+                                <div key={index} style={{ position: "relative" }}>
+                                    <img src={item.source} alt={item.name} className="residentImg" style={{ border: `6px solid ${item.color}` }} />
+                                    <div className='nameContainer' style={{ backgroundColor: item.color }}>
+                                        <h4>{item.name}</h4>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    }
                 </div>
                 <div className='aboutContainer'>
                     <h2 className='aboutTitle'>About</h2>
